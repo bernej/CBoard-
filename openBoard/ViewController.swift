5ea14cb6ae698e09639704b5db5156897517c22e
bernej <jberne@umich.edu>
2017-05-01 14:16:13 -0400

    Reorganizing repo

---
+++ openBoard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  openBoard
+//
+//  Created by Jonathan Berne on 11/2/16.
+//  Copyright © 2016 Hey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
